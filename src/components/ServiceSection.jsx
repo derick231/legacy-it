e34c7da116f69a57433f4bfdc61450b2e76ab9d3
derick <salvationderick23@gmail.com>
2025-12-18 10:53:27 +0545

    added whatsapp link

--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -5,27 +5,27 @@ const ServiceSection = ({ title, content, image, reverse = false }) => {
         className={`
           mx-6 md:mx-10
           flex flex-col md:flex-row
-          items-center
+          group overflow-hidden
+          justify-center
           gap-8
           ${reverse ? "md:flex-row-reverse" : ""}
         `}
       >
-        {/* Text */}
         <div className="w-full md:w-1/2 flex flex-col justify-center">
           <h1 className="text-2xl md:text-3xl font-semibold text-left md:text-center mb-4">
             {title}
           </h1>
-          <p className="text-justify md:text-center text-gray-600">
-            {content}
-          </p>
+          <p className="text-justify md:text-center text-gray-600">{content}</p>
         </div>
 
-        {/* Image */}
-        <img
-          src={image}
-          alt={title}
-          className="w-full md:h-[500px] md:w-1/2 rounded-lg object-cover"
-        />
+        {/* Image Wrapper */}
+        <div className="w-full md:w-[400px] md:h-[400px] overflow-hidden rounded-lg">
+          <img
+            src={image}
+            alt={title}
+            className="w-full h-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-125"
+          />
+        </div>
       </div>
     </section>
   );
